0b49a598ef4744b08a8f31a63771cf22b1222f21
SukkaW <isukkaw@gmail.com>
2019-02-27 21:44:26 +0800

    feat(dns): using old version of uploaded config

--- koolclash/scripts/koolclash_save_dns_config.sh
+++ koolclash/scripts/koolclash_save_dns_config.sh
@@ -8,7 +8,11 @@ touch $KSROOT/koolclash/config/dns.yml
 
 echo $2 | base64 -d | tee $KSROOT/koolclash/config/dns.yml
 
+rm -rf $KSROOT/koolclash/config/config.yml
+cp $KSROOT/koolclash/config/origin.yml $KSROOT/koolclash/config/config.yml
+
 hasdns=$(cat $KSROOT/koolclash/config/config.yml | grep "dns:")
+
 if [[ "$hasdns" != "dns:" ]]; then
     cat $KSROOT/koolclash/config/dns.yml | tee -a $KSROOT/koolclash/config/config.yml
 fi
