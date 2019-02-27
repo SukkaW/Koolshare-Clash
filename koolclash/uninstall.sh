e011047bb348ea12e55070ac524b9ce44095c9e8
SukkaW <isukkaw@gmail.com>
2019-02-27 18:48:25 +0800

    fix(uninstall): stop clash before uninstall

--- koolclash/uninstall.sh
+++ koolclash/uninstall.sh
@@ -3,7 +3,12 @@
 export KSROOT=/koolshare
 source $KSROOT/scripts/base.sh
 
-sleep 1
+if [ -d "$KSROOT/koolclash" ]; then
+    echo_date '停止 KoolClash 以更新。注意更新完成以后 KoolClash 不会自动恢复，需要手动启动 Clash！'
+    sleep 2
+    sh $KSROOT/scripts/koolclash_control.sh stop >/dev/null 2>&1
+    sleep 2
+fi
 
 rm -rf $KSROOT/koolclash
 rm -rf $KSROOT/scripts/koolclash_*
