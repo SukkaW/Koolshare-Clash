513ad00639196cf737f2e03599ac5d821faa81c5
SukkaW <isukkaw@gmail.com>
2019-03-14 00:20:30 +0800

    chore: brand new build tools

--- build/pack.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-tar zcvf koolclash.tar.gz koolclash
--- build/update-dash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/Dreamacro/clash-dashboard/archive/gh-pages.zip
-unzip gh-pages.zip
-rm -rf koolclash/webs/koolclash && mkdir -p koolclash/webs/koolclash
-cp -rf clash-dashboard-gh-pages/* koolclash/webs/koolclash
-rm -rf gh-pages.zip
-rm -rf clash-dashboard-gh-pages
