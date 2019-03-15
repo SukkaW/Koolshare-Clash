e1950bcbecc0fb1aec4a534d97708a7a29a58e13
SukkaW <isukkaw@gmail.com>
2019-03-15 20:52:53 +0800

    fix(uninstall): remove old binary

--- koolclash/uninstall.sh
+++ koolclash/uninstall.sh
@@ -12,6 +12,7 @@ rm -rf $KSROOT/koolclash
 rm -rf $KSROOT/scripts/koolclash_*
 rm -rf $KSROOT/init.d/S99koolclash.sh
 rm -rf $KSROOT/bin/clash
+rm -rf $KSROOT/bin/clash-linux-amd64
 rm -rf $KSROOT/bin/yq
 rm -rf $KSROOT/webs/Module_koolclash.asp
 rm -rf $KSROOT/webs/koolclash
